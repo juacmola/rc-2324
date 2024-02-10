d26947a79b14a4427754b5c23b35956f93e8aa99
juacmola <jorge.u.a@um.es>
2024-02-11 00:22:39 +0100

    Implementación del constructor y parte del run (prueba login)

--- nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/server/NFDirectoryServer.java
+++ nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/udp/server/NFDirectoryServer.java
@@ -1,10 +1,12 @@
 package es.um.redes.nanoFiles.udp.server;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Random;
@@ -38,7 +40,7 @@ public class NFDirectoryServer {
 	 * TODO: Añadir aquí como atributos las estructuras de datos que sean necesarias
 	 * para mantener en el directorio cualquier información necesaria para la
 	 * funcionalidad del sistema nanoFilesP2P: ficheros publicados, servidores
-	 * registrados, etc.
+	 * registrados, etc. Luego, inicializarlas.
 	 */
 
 
@@ -55,21 +57,17 @@ public class NFDirectoryServer {
 	private double messageDiscardProbability;
 
 	public NFDirectoryServer(double corruptionProbability) throws SocketException {
-		/*
-		 * Guardar la probabilidad de pérdida de datagramas (simular enlace no
-		 * confiable)
-		 */
+		/* Guardar la probabilidad de pérdida de datagramas (simular enlace no
+		 * confiable) */
 		messageDiscardProbability = corruptionProbability;
-		/*
-		 * TODO: (Boletín UDP) Inicializar el atributo socket: Crear un socket UDP
-		 * ligado al puerto especificado por el argumento directoryPort en la máquina
-		 * local,
-		 */
-		/*
-		 * TODO: (Boletín UDP) Inicializar el resto de atributos de esta clase
-		 * (estructuras de datos que mantiene el servidor: nicks, sessionKeys, etc.)
-		 */
-
+		/* DONE: (Boletín UDP) Inicializar el atributo socket: Crear un socket UDP
+		 * ligado al puerto especificado por el argumento directoryPort en la máquina local */
+		socket = new DatagramSocket(DIRECTORY_PORT);
+		
+		/* DONE: (Boletín UDP) Inicializar el resto de atributos de esta clase
+		 * (estructuras de datos que mantiene el servidor: nicks, sessionKeys, etc.) */
+		this.nicks = new HashMap<>();
+		this.sessionKeys = new HashMap<>();
 
 
 		if (NanoFiles.testMode) {
@@ -85,27 +83,22 @@ public class NFDirectoryServer {
 		byte[] receptionBuffer = null;
 		InetSocketAddress clientAddr = null;
 		int dataLength = -1;
-		/*
-		 * TODO: (Boletín UDP) Crear un búfer para recibir datagramas y un datagrama
-		 * asociado al búfer
-		 */
-
-
-
-
+		
+		/* DONE: (Boletín UDP) Crear un búfer para recibir datagramas y un datagrama
+		 * asociado al búfer */
+		receptionBuffer = new byte[DirMessage.PACKET_MAX_SIZE];
+		DatagramPacket packetFromClient = new DatagramPacket(receptionBuffer, receptionBuffer.length);
 		System.out.println("Directory starting...");
 
 		while (true) { // Bucle principal del servidor de directorio
-
-			// TODO: (Boletín UDP) Recibimos a través del socket un datagrama
-
-			// TODO: (Boletín UDP) Establecemos dataLength con longitud del datagrama
-			// recibido
-
-			// TODO: (Boletín UDP) Establecemos 'clientAddr' con la dirección del cliente,
-			// obtenida del
-			// datagrama recibido
-
+			// DONE: (Boletín UDP) Recibimos a través del socket un datagrama
+			System.out.println("Waiting to receive datagram...");						// Opcional
+			socket.receive(packetFromClient);
+			// DONE: (Boletín UDP) Establecemos dataLength con longitud del datagrama recibido
+			dataLength = packetFromClient.getLength();
+			// DONE: (Boletín UDP) Establecemos 'clientAddr' con la dirección del cliente,
+			// obtenida del datagrama recibido
+			clientAddr = (InetSocketAddress) packetFromClient.getSocketAddress();
 
 
 
@@ -118,31 +111,31 @@ public class NFDirectoryServer {
 			}
 			System.out.println("Directory received datagram from " + clientAddr + " of size " + dataLength + " bytes");
 
-			// Analizamos la solicitud y la procesamos
-			if (dataLength > 0) {
+			if (dataLength > 0) {	// Analizamos la solicitud y la procesamos
 				String messageFromClient = null;
-				/*
-				 * TODO: (Boletín UDP) Construir una cadena a partir de los datos recibidos en
-				 * el buffer de recepción
-				 */
-
-
-
+				
+				/* DONE: (Boletín UDP) Construir una cadena a partir de los datos recibidos en
+				 * el buffer de recepción */
+				messageFromClient = new String(receptionBuffer, 0, packetFromClient.getLength());
 
 				if (NanoFiles.testMode) { // En modo de prueba (mensajes en "crudo", boletín UDP)
 					System.out.println("[testMode] Contents interpreted as " + dataLength + "-byte String: \""
 							+ messageFromClient + "\"");
-					/*
-					 * TODO: (Boletín UDP) Comprobar que se ha recibido un datagrama con la cadena
-					 * "login" y en ese caso enviar como respuesta un mensaje al cliente con la
-					 * cadena "loginok". Si el mensaje recibido no es "login", se informa del error
-					 * y no se envía ninguna respuesta.
-					 */
+					
+					/* DONE: (Boletín UDP) Comprobar que se ha recibido un datagrama con la cadena "login" y en 
+					 * ese caso enviar como respuesta un mensaje al cliente con la cadena "loginok". Si el 
+					 * mensaje recibido no es "login", se informa del error y no se envía ninguna respuesta. */
+					if(messageFromClient.equals("login")) {
+						String messageToClient = new String("loginok");
+						byte[] dataToClient = messageToClient.getBytes();
+						System.out.println("Sending datagram with message \"" + messageToClient + "\"");			// Opcional
+						System.out.println("Destination is client at addr: " + clientAddr);							// Opcional
+						DatagramPacket packetToClient = new DatagramPacket(dataToClient, dataToClient.length, clientAddr);
+						socket.send(packetToClient);
+					}
+					else System.out.println("The message is not a 'login' message");
 
-
-
-				} else { // Servidor funcionando en modo producción (mensajes bien formados)
-
+				} else { 	// Servidor funcionando en modo producción (mensajes bien formados)
 					// Vemos si el mensaje debe ser ignorado por la probabilidad de descarte
 					double rand = Math.random();
 					if (rand < messageDiscardProbability) {
