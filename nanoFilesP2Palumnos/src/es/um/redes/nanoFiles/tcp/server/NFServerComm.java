5164167e27533500262d14a968ab381901df93dd
juacmola <jorge.u.a@um.es>
2024-03-17 18:57:39 +0100

    Recibe un entero a través del socket y luego lo enviar de vuelta

--- nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/tcp/server/NFServerComm.java
+++ nanoFilesP2Palumnos/src/es/um/redes/nanoFiles/tcp/server/NFServerComm.java
@@ -17,23 +17,24 @@ import es.um.redes.nanoFiles.util.FileInfo;
 
 public class NFServerComm {
 
-	public static void serveFilesToClient(Socket socket) {
-		/*
-		 * TODO: Crear dis/dos a partir del socket
-		 */
-		/*
-		 * TODO: Mientras el cliente esté conectado, leer mensajes de socket,
+	public static void serveFilesToClient(Socket socket) throws IOException {
+		/* DONE: Crear dis/dos a partir del socket */
+		DataInputStream dis = new DataInputStream(socket.getInputStream());
+		DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+		
+		/* TODO: Mientras el cliente esté conectado, leer mensajes de socket,
 		 * convertirlo a un objeto PeerMessage y luego actuar en función del tipo de
-		 * mensaje recibido, enviando los correspondientes mensajes de respuesta.
-		 */
-		/*
-		 * TODO: Para servir un fichero, hay que localizarlo a partir de su hash (o
+		 * mensaje recibido, enviando los correspondientes mensajes de respuesta.*/
+		while (socket.isConnected()) {
+			int dataFromClient = dis.readInt();
+			dos.writeInt(dataFromClient);
+		}
+		/* TODO: Para servir un fichero, hay que localizarlo a partir de su hash (o
 		 * subcadena) en nuestra base de datos de ficheros compartidos. Los ficheros
 		 * compartidos se pueden obtener con NanoFiles.db.getFiles(). El método
 		 * FileInfo.lookupHashSubstring es útil para buscar coincidencias de una
 		 * subcadena del hash. El método NanoFiles.db.lookupFilePath(targethash)
-		 * devuelve la ruta al fichero a partir de su hash completo.
-		 */
+		 * devuelve la ruta al fichero a partir de su hash completo. */
 
 
 
